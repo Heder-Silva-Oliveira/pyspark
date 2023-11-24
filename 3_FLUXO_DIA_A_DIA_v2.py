b177b3cf3bf790e08e635b39893cc6ddc369308e
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2023-11-24 10:57:22 -0300

    Limpar

--- 3_FLUXO_DIA_A_DIA_v2.py
+++ 3_FLUXO_DIA_A_DIA_v2.py
@@ -179,12 +179,5 @@ if  status == 1:
 fluxo_dia_a_dia.show()
 
 fluxo_dia_a_dia.write.jdbc(url=mysql_url, table='fluxo_caixa_diario', mode='overwrite', properties=mysql_properties)
-# COMMAND ----------
-
-#fluxo_dia_a_dia.write.csv("/FileStore/tables/csv/FLUXO_DIA_A_DIA.csv", header=True, mode="overwrite")
-
-#dbutils.fs.rm("dbfs:/FileStore/tables/csv/Fluxo_M2.csv", recurse=True)
-
-# COMMAND ----------
 
 spark.stop()
